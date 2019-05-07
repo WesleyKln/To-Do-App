a911b67849cbea33a6c0cd8a237bd4a920d1fb0e
WesleyKln <contact@wesleyklein.fr>
2019-05-07 11:55:15 +0200

    Added some styling

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-## To Do App / Pure JavaScript
-Création d'une petite application Web qui permet d'ajouter des produits sous forme de liste avec possibilité de supprimer chaque entrée de manière indépendante !
+![Capture d’écran de 2019-05-07 11-43-46](https://user-images.githubusercontent.com/42122563/57290750-5c879f80-70be-11e9-9253-48ac0889bff4.jpg)
+# To Do App
+### Pure JavaScript
+Petite application Web qui permet d'ajouter des produits sous forme de liste avec possibilité de supprimer chaque entrée de manière indépendante !
--- app.js
+++ app.js
@@ -3,14 +3,18 @@ document.addEventListener("DOMContentLoaded", function() {
     var buttonToDo = document.getElementById('buttonToDo');
     var inputField = document.getElementById('inputToDo');
     
-    // J'écoute le click sur le bouton Ajouter
+    // J'écoute le clic sur le bouton Ajouter
     buttonToDo.addEventListener("click", function() {
         
         // Je récupère la valeur de l'utilisateur    
         var inputValue = inputField.value;
         inputField.value = "";
 
-        //Création d'un li avec la valeur de l'utilisateur stockée dedans
+        // Ne pas ajouter d'entrée si le champ est vide
+        if (inputValue === "") {
+            return false;
+        }
+        // Création d'un li avec la valeur de l'utilisateur stockée dedans
         var newToDo = document.createElement('li');
         newToDo.setAttribute("class", "list");
         var newText = document.createTextNode(inputValue);
@@ -18,9 +22,9 @@ document.addEventListener("DOMContentLoaded", function() {
         var position = document.getElementById('toDoList');
         position.appendChild(newToDo);
 
-        //Ajouter le bouton Remove
+        // Ajouter le bouton Remove
         var removeButton = document.createElement('button');
-        var removeText = document.createTextNode('Supprimer');
+        var removeText = document.createTextNode('');
         removeButton.appendChild(removeText);
         removeButton.setAttribute("class", "removeButton");
         newToDo.appendChild(removeButton);
